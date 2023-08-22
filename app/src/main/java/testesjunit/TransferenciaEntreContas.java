c128b8b345791269c71c79ee7261535bdb8b72ea
Caique Souza Fonseca <c.souzaf@hotmail.com>
2023-08-22 15:51:43 -0300

    Junit Exceptions Test

---
+++ app/src/main/java/testesjunit/TransferenciaEntreContas.java
@@ -0,0 +1,13 @@
+package testesjunit;
+
+public class TransferenciaEntreContas {
+    
+    public void transfere(Conta contaOrigem, Conta contaDestino, int valor){
+
+        //Use debug here for to valid after create ExcpetionsTest
+        if(valor <= 0){
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+    }
+
+}
--- app/src/test/java/testesjunit/ExceptionsTest.java
+++ app/src/test/java/testesjunit/ExceptionsTest.java
@@ -1,5 +1,34 @@
 package testesjunit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExceptionsTest {
     
+    @Test
+    void validarLancamentoExcecaoTrasferencia(){
+
+       Conta contaOrigem = new Conta("201023", 0);
+       Conta contaDestino = new Conta("21846", 100);
+
+       TransferenciaEntreContas transferenciaEntreContas = new TransferenciaEntreContas();
+       
+       Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            transferenciaEntreContas.transfere(contaOrigem, contaDestino, -1); 
+       });
+    
+    }
+
+
+    @Test
+    void verificarSeTransferenciaNaoVaiLancarExcecao(){
+
+       Conta contaOrigem = new Conta("201023", 0);
+       Conta contaDestino = new Conta("21846", 100);
+
+       TransferenciaEntreContas transferenciaEntreContas = new TransferenciaEntreContas();
+       
+        Assertions.assertDoesNotThrow(() -> transferenciaEntreContas.transfere(contaOrigem, contaDestino, -1));
+    
+    }
 }
